fb7075f19e2fbf6b2993053a7e845c0bd69f15cf
Pavel Karateev <lancelote.du.lac@gmail.com>
2018-05-19 10:25:20 +0300

    Chapter 2 is finished

---
+++ begginners/02_writing_and_debugging_scripts/home_and_term.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# The script should display the path to your homedirectory and the terminal
+# type that you are using
+
+echo "Home directory path: ${HOME}"
+echo "Terminal: ${TERM}"
